dc13fb0a74c0b107ad37c5e200f14ed9964044f8
Juan Giler <juangiler7@outlook.com>
2020-08-06 00:26:07 -0500

    docs agregado

---
+++ docs/precache-manifest.7dfe0fd3063d13d75a57e20d5322fa06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59e6b09d0e5045cc0ba22d72eb85a57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a33a4ce7ab234d3be59c",
+    "url": "/static/css/main.b007e606.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a33a4ce7ab234d3be59c",
+    "url": "/static/js/main.6dd7f11c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
